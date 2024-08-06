962c6d8a9b662f6e2e26ea35468e399f80ba2793
Imsharad <im.sharad.jain@gmail.com>
2024-08-06 15:33:53 +0530

    Update auth.py and connection.py to load environment variables from .env. It ensures sensitive data security.

--- utils/auth.py
+++ utils/auth.py
@@ -1,16 +1,28 @@
 import requests
 from utils.time import timeit
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
 
 @timeit()
 def get_clientell_token():
-    url = "https://rev-prod-k8s.clientellone.com/clientell/api/user/login"
-    body = {"email": "ruthuparna@getclientell.com", "password": "Clientell@123"}
+    url = os.getenv('CLIENTELL_LOGIN_URL')
+    email = os.getenv('CLIENTELL_EMAIL')
+    password = os.getenv('CLIENTELL_PASSWORD')
+    body = {"email": email, "password": password}
     response = requests.post(url, json=body)
     return response.json()["access_token"]
 
 @timeit()
 def get_salesforce_token(clientell_token):
-    url = "https://rev-prod-k8s.clientellone.com/api/salesforce/getAccessToken"
+    url = os.getenv('SALESFORCE_TOKEN_URL')
     headers = {"Authorization": f"Token {clientell_token}"}
     response = requests.get(url, headers=headers)
     return response.json()["access_token"]
+
+
+    CLIENTELL_LOGIN_URL = "https://rev-prod-k8s.clientellone.com/clientell/api/user/login"
+    CLIENTELL_EMAIL = {"email": "ruthuparna@getclientell.com", "CLIENTELL_PASSWORD": "Clientell@123"}
+    SALESFORCE_TOKEN_URL = "https://rev-prod-k8s.clientellone.com/api/salesforce/getAccessToken"
--- utils/connection.py
+++ utils/connection.py
@@ -2,6 +2,11 @@ from simple_salesforce import Salesforce
 from utils.auth import get_clientell_token, get_salesforce_token
 import cachetools.func
 from utils.time import timeit
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
 
 @timeit()
 @cachetools.func.ttl_cache(maxsize=1, ttl=600)
@@ -9,7 +14,7 @@ def get_salesforce_connection():
     clientell_token = get_clientell_token()
     salesforce_token = get_salesforce_token(clientell_token)
     sf = Salesforce(
-        instance_url="https://clientell4-dev-ed.my.salesforce.com",
+        instance_url=os.getenv('SALESFORCE_INSTANCE_URL'),
         session_id=salesforce_token,
     )
     return sf
